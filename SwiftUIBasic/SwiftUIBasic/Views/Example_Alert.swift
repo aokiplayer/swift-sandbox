b0040be1a68f980d90c2cef292e95f0641ecbb77
Hiroyuki Aoki <aokiplayer@icloud.com>
2022-07-26 11:47:57 +0900

    Alert

---
+++ SwiftUIBasic/SwiftUIBasic/Views/Example_Alert.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct ExampleAlert: View {
+    // アラートダイアログの表示を制御する State
+    @State var isAlertShown: Bool
+
+    var body: some View {
+        VStack {
+            Button {
+                // ボタンをタップしたら State を更新
+                isAlertShown.toggle()
+            } label: {
+                Label("Show Alert", systemImage: "bubble.right")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .symbolRenderingMode(.multicolor)
+            }
+            .buttonStyle(.borderedProminent)
+            .frame(width: 200, height: 56)
+            // State の値をもとにダイアログを表示
+            .alert("Hello", isPresented: $isAlertShown) {
+                Button("OK") {
+                    print("OK が押されたよ。")
+                }
+            } message: {
+                Text("OK をタップしたら閉じます。")
+            }
+        }
+    }
+}
+
+struct Example_Alert_Previews: PreviewProvider {
+    static var previews: some View {
+        ExampleAlert(isAlertShown: false)
+    }
+}
