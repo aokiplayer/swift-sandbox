1ecca5dd4688502327f6150fbd079e2666ba98c5
Hiroyuki Aoki <aokiplayer@icloud.com>
2022-09-15 14:29:43 +0900

    Index

--- SwiftUIBasic/SwiftUIBasic/ContentView.swift
+++ SwiftUIBasic/SwiftUIBasic/ContentView.swift
@@ -3,45 +3,81 @@ import SwiftUI
 struct ContentView: View {
     // body は常に単一のビューを返すように定義する
     var body: some View {
-        /*
-         以下のコメントを切り替えて確認しましょう
-         */
-        // 別のファイルで定義したビューを表示
-//        ExampleText()
-//        ExampleVStack()
-//        ExampleHStack()
-//        ExampleZStack()
-//        ExampleList1()
-//        ExampleList2()
-//        ExampleModal()
-//        ExampleSafariVC()
-//        ExampleLabel()
-//        ExampleGroup()
-//        ExampleTimelineView()
-//        ExamplePreview1()
-//        ExamplePreview2()
-//        ExampleAttributedString()
-//        ExampleNavigationStack1()
-//        ExampleNavigationStack2()
-//        ExampleAlert1(isAlertShown: false)
-//        ExampleAlert2(isAlertShown: false)
-//        ExampleAlert3()
-//        ExampleBinding()
-//        ExampleFont()
-//        ExampleObserve1(person: Person(name: "山田二郎", age: 53))
-//        ExampleObserve2()
-//        ExampleEnvironmentObject()
-//            .environmentObject(
-//                MyAppColors(myBackgroundColor: .gray,
-//                            myFontColor: .blue))
-//        ExampleStateAndObserved()
-//        ExampleEnvironment()
-//            .environment(\.locale, Locale(identifier: "ja_JP"))
-//            .environment(\.colorScheme, .dark)
-//        ExampleAppStorage()
-//        ExampleGeometry1()
-        ExampleTextField()
-            .environment(\.locale, Locale(identifier: "ja_JP"))
+        NavigationStack {
+            List(views, id: \.title) { element in
+                row(value: element)
+                    .padding()
+            }
+            .navigationTitle("SwiftUI サンプル")
+        }
+    }
+    
+    private func row(value: RowValue) -> some View {
+        NavigationLink {
+            value.view
+        } label: {
+            HStack {
+                VStack(alignment: .leading) {
+                    Label(value.title, systemImage: "iphone")
+                        .font(.title2)
+                    
+                    Text(value.note)
+                        .font(.caption)
+                        .padding(.top, 8)
+                }
+            }
+        }
+    }
+}
+
+typealias RowValue = (title: String, view: AnyView, note: String)
+
+// ビューを配列に持たせる場合は、AnyView にラップして型消去する必要あり
+// some View 型だと、実際の型情報を保持しているため？
+fileprivate var views: [RowValue] = [
+    ("Text", AnyView(ExampleText()), ""),
+    ("VStack", AnyView(ExampleVStack()), ""),
+    ("HStack", AnyView(ExampleHStack()), ""),
+    ("ZStack", AnyView(ExampleZStack()), ""),
+    ("List", AnyView(ExampleList1()), ""),
+    ("List2", AnyView(ExampleList2()), ""),
+    ("Modal", AnyView(ExampleModal()), ""),
+    ("SafariVC", AnyView(ExampleSafariVC()), ""),
+    ("Label", AnyView(ExampleLabel()), ""),
+    ("Group", AnyView(ExampleGroup()), ""),
+    ("TimelineView", AnyView(ExampleTimelineView()), ""),
+    ("Preview1", AnyView(ExamplePreview1()), "プレビューのサイズを指定しているため、個別のビューファイルで確認してください。"),
+    ("Preview2", AnyView(ExamplePreview2()), "プレビューのサイズを指定しているため、個別のビューファイルで確認してください。"),
+    ("AttributedString", AnyView(ExampleAttributedString()), ""),
+    ("NavigationStack1", AnyView(ExampleNavigationStack1()), ""),
+    ("NavigationStack2", AnyView(ExampleNavigationStack2()), ""),
+    ("Alert1", AnyView(ExampleAlert1(isAlertShown: false)), ""),
+    ("Alert2", AnyView(ExampleAlert2(isAlertShown: false)), ""),
+    ("Alert3", AnyView(ExampleAlert3()), ""),
+    ("Binding", AnyView(ExampleBinding()), ""),
+    ("Font", AnyView(ExampleFont()), ""),
+    ("Observe1", AnyView(ExampleObserve1(person: Person(name: "山田二郎", age: 53))), ""),
+    ("Observe2", AnyView(ExampleObserve2()), ""),
+    ("EnvironmentObject", AnyView(ExampleEnvironmentObject()
+        .environmentObject(
+            MyAppColors(myBackgroundColor: .gray,
+                        myFontColor: .blue))), ""),
+    ("StateAndObserved", AnyView(ExampleStateAndObserved()), ""),
+    ("Environment", AnyView(ExampleEnvironment()
+        .environment(\.locale, Locale(identifier: "ja_JP"))
+        .environment(\.colorScheme, .dark)), ""),
+    ("AppStorage", AnyView(ExampleAppStorage()), ""),
+    ("Geometry1", AnyView(ExampleGeometry1()), ""),
+    ("TextField", AnyView(ExampleTextField()
+        .environment(\.locale, Locale(identifier: "ja_JP"))), ""),
+]
+
+// AnyView の配列を List で繰り返すので、Identifiable に準拠
+extension AnyView: Identifiable {
+    public typealias ID = UUID
+
+    public var id: UUID {
+        UUID()
     }
 }
 
