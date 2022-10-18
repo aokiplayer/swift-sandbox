b4b0d48493f5da53aecbe46475edf6d7f645f61b
Hiroyuki Aoki <aokiplayer@icloud.com>
2022-10-18 16:37:32 +0900

    PhotoPicker from iOS 16
    How do we treat multiple selected image?

---
+++ SwiftUIBasic/SwiftUIBasic/Views/Photos/Example_PhotoPicker.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ExamplePhotoPicker: View {
+    var body: some View {
+        Text("A")
+    }
+}
+
+struct Example_PhotoPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ExamplePhotoPicker()
+    }
+}
