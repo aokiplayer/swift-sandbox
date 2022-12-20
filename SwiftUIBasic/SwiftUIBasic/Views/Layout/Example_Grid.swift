5c88d16329c9fdb7c39089b4118c0ed644902c2e
Hiroyuki Aoki <aokiplayer@icloud.com>
2022-12-20 13:51:37 +0900

    Grid from iOS 16

---
+++ SwiftUIBasic/SwiftUIBasic/Views/Layout/Example_Grid.swift
@@ -0,0 +1,20 @@
+//
+//  Example_Grid.swift
+//  SwiftUIBasic
+//
+//  Created by Hiroyuki Aoki on 2022/12/20.
+//
+
+import SwiftUI
+
+struct Example_Grid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Example_Grid_Previews: PreviewProvider {
+    static var previews: some View {
+        Example_Grid()
+    }
+}
